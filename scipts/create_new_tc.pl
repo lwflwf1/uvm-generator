0111e06130836541971b67382b4a20d8f0cb8895
gong <you@example.com>
2020-04-24 14:23:28 +0800

    create_new_tc: generate more than one case

---
+++ scipts/create_new_tc.pl
@@ -0,0 +1,17 @@
+#! /usr/bin/perl -w
+if(@ARGV < 2) {
+    die "not enough argument\n";
+}
+my $src = "my_case".$ARGV[0];
+my @dst = @ARGV[1..$#ARGV];
+foreach (@dst) {
+    my $dst = "my_case".$_;
+    mkdir "./tc/$dst";
+    system "cp -a ./tc/$src/$src.sv ./tc/$dst/$dst.sv";
+    system "cp -a ./tc/$src/$src.cfg ./tc/$dst/$dst.cfg";
+    my $now = join "-", ((localtime)[5]+1900, ((localtime)[4]+1, (localtime)[3]));
+    system "perl -p -i -e \"s/$src/$dst/gi;s/(create time.*?:).*/\\1 $now/\" ./tc/$dst/$dst.sv";
+    system "perl -p -i -e \"s/$src/$dst/gi;s/(create time.*?:).*/\\1 $now/\" ./tc/$dst/$dst.cfg";
+    print "generate $dst\n";
+}
+
